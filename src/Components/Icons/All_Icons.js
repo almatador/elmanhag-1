194c537a15cfaa86cdde099c64882303993e05d9
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-06-27 22:44:20 +0300

    New Update

---
+++ src/Components/Icons/All_Icons.js
@@ -0,0 +1,13 @@
+export { default as CurriculaIcon } from "./CurriculaIcon.jsx";
+
+export { default as DutiesIcon } from "./DutiesIcon.jsx";
+
+export { default as FinalReviewsIcon } from "./FinalReviewsIcon.jsx";
+
+export { default as HomeIcon } from "./HomeIcon.jsx";
+
+export { default as LiveClassesIcon } from "./LiveClassesIcon.jsx";
+
+export { default as MonthsReviewsIcon } from "./MonthsReviewsIcon.jsx";
+
+export { default as SolveExamsIcon } from "./SolveExamsIcon.jsx";
