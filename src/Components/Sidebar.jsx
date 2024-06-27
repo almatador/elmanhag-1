96c73abfaf31c394ca07b91c78c18e2bc4f4bdd8
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-06-27 22:44:17 +0300

    New Update

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import { CurriculaIcon } from "./Icons/All_Icons";
 const Sidebar = () => {
        return (
               <>
                      <span>Sidebar</span>
+                     <CurriculaIcon />
               </>
        );
 };
