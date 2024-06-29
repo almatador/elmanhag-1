fdc7bd762b266cff4f11816710dd125f0b77747d
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-06-29 15:36:15 +0300

    New Update

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { CurriculaIcon } from "./Icons/All_Icons";
+import logo from "../assets/Images/logoElmanhag.png"
+// import { CurriculaIcon } from "./Icons/All_Icons";
 const Sidebar = () => {
        return (
               <>
-                     <span>Sidebar</span>
-                     <CurriculaIcon />
+                     <aside className="bg-mainColor w-[20%] h-full">
+                            <img src={logo} alt="logo" />
+                            <div className="">asdasd</div>
+                            {/* <CurriculaIcon /> */}
+                     </aside>
               </>
        );
 };
