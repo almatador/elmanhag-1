18fc44adc7a3f04b93e9b4868b0d9ce4cc1d981e
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-06-30 15:45:43 +0300

    New Update

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -4,7 +4,7 @@ import MenuSide from "./MenuSide.jsx";
 const Sidebar = () => {
        return (
               <>
-                     <aside className="w-[20%] h-full overflow-hidden flex flex-col items-center justify-between bg-mainColor ">
+                     <aside className="w-[20%] h-full overflow-hidden sticky top-0 flex flex-col items-center justify-between bg-mainColor ">
                             <LogoSidebar Width="100%" />
                             <MenuSide />
                      </aside>
