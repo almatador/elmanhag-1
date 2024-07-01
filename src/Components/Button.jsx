b1f806d8d09ba52cc618b4a92e50cfbc02faf436
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-07-01 14:20:38 +0300

    New Update

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = ({ Text, BgColor, Color, Size }) => {
   return (
     <>
-      <button type='button' className={`bg-[${BgColor}] text-[${Color}] text-${Size} font-medium rounded-lg px-7 pt-2 py-3`}>{Text}</button>
+      <button type='button' className={`bg-[${BgColor}] text-white text-${Size} font-medium rounded-lg px-7 pt-2 py-3`}>{Text}</button>
     </>
   )
 }
