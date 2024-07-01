fda030c9728ee7a8e25724511ef1551d9fc54eec
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-07-01 14:21:00 +0300

    New Update

--- src/Pages/DashboardPage/DashboardPage.jsx
+++ src/Pages/DashboardPage/DashboardPage.jsx
@@ -1,18 +1,8 @@
-import React from 'react'
-// import FinalReviewsIcon from '../../Components/Icons/FinalReviewsIcon'
-// import HomeIcon from '../../Components/Icons/HomeIcon'
-// import LiveClassesIcon from '../../Components/Icons/LiveClassesIcon'
-// import MonthsReviewsIcon from '../../Components/Icons/MonthsReviewsIcon'
-// import SolveExamsIcon from '../../Components/Icons/SolveExamsIcon'
+import React, { useContext } from 'react'
 
 const DashboardPage = () => {
        return (
               <>
-                     {/* <FinalReviewsIcon Width='50' Height='50' />
-                     <HomeIcon Width='50' Height='50' />
-                     <LiveClassesIcon Width='50' Height='50' />
-                     <MonthsReviewsIcon Width='50' Height='50' />
-                     <SolveExamsIcon Width='50' Height='50' /> */}
                      <div>DashboardPage</div>
               </>
        )
