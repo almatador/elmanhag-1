d414ab38e8cca7f70d1c18b3969062c2083402e2
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-07-02 09:50:37 +0300

    New Update

--- src/App.jsx
+++ src/App.jsx
@@ -5,48 +5,7 @@ import { Dashboard } from './Layouts/AllLayouts'
 
 
 function App() {
-  const [student, setStudent] = useState(null);
 
-  useEffect(() => {
-    const getToken = async () => {
-      try {
-        const response = await axios.get('https://my.elmanhag.shop/api/user_token', {
-        });
-
-        setStudent(response.data);
-        console.log(response.data);
-        localStorage.setItem("token", response.data.token)
-      } catch (error) {
-        console.error('Student data fetch error:', error);
-      }
-    };
-    getToken();
-
-    const fetchStudentData = async () => {
-      try {
-        // Retrieve token from localStorage or wherever it is stored
-        const token = localStorage.getItem('token');
-
-        // Make a GET request to fetch student data
-        const response = await axios.post('https://my.elmanhag.shop/api/user_data', {
-          headers: {  
-            Authorization: `Bearer ${token}`,
-          }
-        });
-
-        // Set student state with the data received
-        // console.log(response.data);
-      } catch (error) {
-        console.error('Student data fetch error:', error);
-      }
-    };
-    fetchStudentData();
-
-  }, []);
-
-  // if (!student) {
-  //   return <div>Loading...</div>;
-  // }
   return (
     <>
       <div className="flex gap-x-4 flex-col">
