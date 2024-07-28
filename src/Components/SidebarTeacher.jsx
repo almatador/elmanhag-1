dec07d5aa2f1738e148dfc02171e9c8d210ab149
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-07-28 13:09:01 +0300

    New UPdate A

---
+++ src/Components/SidebarTeacher.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import LogoSidebar from "../../public/assets/Images/logoSidebar.jsx"
+import MenuSide from "./MenuSide.jsx";
+const SidebarTeacher = () => {
+       return (
+              <>
+                     <aside className="w-1/5 fixed h-screen right-0 overflow-hidden flex flex-col items-center bg-mainColor gap-y-6">
+                            <div className="h-1/5 mt-2">
+                                   <LogoSidebar Width="100%" />
+                            </div>
+                            <MenuSide />
+                     </aside>
+              </>
+       );
+};
+
+export default SidebarTeacher;
