5c439bb7d2e5ae7c2ebae2638a04191f36b29b29
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-07-28 13:09:08 +0300

    New UPdate A

--- src/Components/Icons/All_Icons.js
+++ src/Components/Icons/All_Icons.js
@@ -1,13 +1,13 @@
-export { default as CurriculaIcon } from "./CurriculaIcon.jsx";
+export { default as CurriculaIcon } from "./StudentIcons/CurriculaIcon.jsx";
 
-export { default as DutiesIcon } from "./DutiesIcon.jsx";
+export { default as DutiesIcon } from "./StudentIcons/DutiesIcon.jsx";
 
-export { default as FinalReviewsIcon } from "./FinalReviewsIcon.jsx";
+export { default as FinalReviewsIcon } from "./StudentIcons/FinalReviewsIcon.jsx";
 
-export { default as HomeIcon } from "./HomeIcon.jsx";
+export { default as HomeIcon } from "./StudentIcons/HomeIcon.jsx";
 
-export { default as LiveClassesIcon } from "./LiveClassesIcon.jsx";
+export { default as LiveClassesIcon } from "./StudentIcons/LiveClassesIcon.jsx";
 
-export { default as MonthsReviewsIcon } from "./MonthsReviewsIcon.jsx";
+export { default as MonthsReviewsIcon } from "./StudentIcons/MonthsReviewsIcon.jsx";
 
-export { default as SolveExamsIcon } from "./SolveExamsIcon.jsx";
+export { default as SolveExamsIcon } from "./StudentIcons/SolveExamsIcon.jsx";
