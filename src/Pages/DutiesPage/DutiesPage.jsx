a0adf0d8ed4e122acb522d1b7bcea748ce8fd773
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-07-28 13:10:06 +0300

    New UPdate A

--- src/Pages/DutiesPage/DutiesPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const DutiesPage = () => {
-       return (
-              <div>DutiesPage</div>
-       )
-}
-
-export default DutiesPage
--- src/Pages/LiveClassesPage/LiveClassesPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const LiveClassesPage = () => {
-       return (
-              <div>LiveClassesPage</div>
-       )
-}
-
-export default LiveClassesPage
