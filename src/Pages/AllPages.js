d45b15735914c1c4e5835a1f1ce1129c981b2701
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-07-28 13:09:57 +0300

    New UPdate A

--- src/Pages/AllPages.js
+++ src/Pages/AllPages.js
@@ -1,6 +1,13 @@
-export { default as CurriculaPage } from "./CurriculaPage/CurriculaPage";
-export { default as DutiesPage } from "./DutiesPage/DutiesPage";
-export { default as LiveClassesPage } from "./LiveClassesPage/LiveClassesPage";
-export { default as MonthsreviewsPage } from "./MonthsReviewsPage/MonthsReviewsPage";
-export { default as FinalReviewsPage } from "./FinalReviewsPage/FinalReviewsPage";
-export { default as SolveExamsPage } from "./SolveExamsPage/SolveExamsPage";
+/* Super Admin Page */
+/* Admin Page */
+/* Teachers Page */
+/* Parent Page */
+/* Affiliate Page */
+
+/* Student Page */
+export { default as CurriculaPage } from "./Student/CurriculaPage/CurriculaPage";
+export { default as DutiesPage } from "./Student/DutiesPage/DutiesPage";
+export { default as LiveClassesPage } from "./Student/LiveClassesPage/LiveClassesPage";
+export { default as MonthsreviewsPage } from "./Student/MonthsReviewsPage/MonthsReviewsPage";
+export { default as FinalReviewsPage } from "./Student/FinalReviewsPage/FinalReviewsPage";
+export { default as SolveExamsPage } from "./Student/SolveExamsPage/SolveExamsPage";
