0b34db08f91839cf8c76f001f2ea053e476b078a
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-07-29 15:48:10 +0300

    New Update A

--- src/Components/LinksHomeDS.jsx
+++ src/Components/LinksHomeDS.jsx
@@ -1,125 +1,56 @@
 import React, { useState } from "react";
 
-import {
-       CurriculaIcon,
-       DutiesIcon,
-       FinalReviewsIcon,
-       HomeIcon,
-       LiveClassesIcon,
-       MonthsReviewsIcon,
-       SolveExamsIcon,
-} from "./Icons/All_Icons";
 import { NavLink } from "react-router-dom";
 
 
-const MenuSide = () => {
+const LinksHomeDS = () => {
        const [isActiveHome, setIsActiveHome] = useState(true);
-       const [isActiveCurricula, setIsActiveCurricula] = useState(false);
-       const [isActiveDuties, setIsActiveDuties] = useState(false);
-       const [isActiveLiveClasses, setIsActiveLiveClasses] = useState(false);
-       const [isActiveMonthsReviews, setIsActiveMonthsReviews] = useState(false);
-       const [isActiveFinalReviews, setIsActiveFinalReviews] = useState(false);
-       const [isActiveSolveExams, setIsActiveSolveExams] = useState(false);
+       const [isActiveLevels, setIsActiveLevels] = useState(false);
+       const [isActiveAboutUs, setIsActiveAboutUs] = useState(false);
+       const [isActiveConectUs, setIsActiveConectUs] = useState(false);
 
        const handleClickHome = () => {
               setIsActiveHome(!isActiveHome);
-              setIsActiveCurricula(false);
-              setIsActiveDuties(false);
-              setIsActiveLiveClasses(false);
-              setIsActiveMonthsReviews(false);
-              setIsActiveFinalReviews(false);
-              setIsActiveSolveExams(false);
+              setIsActiveLevels(false);
+              setIsActiveAboutUs(false);
+              setIsActiveConectUs(false);
        };
-       const handleClickCurricula = () => {
+       const handleClickLevels = () => {
               setIsActiveHome(false);
-              setIsActiveCurricula(!isActiveCurricula);
-              setIsActiveDuties(false);
-              setIsActiveLiveClasses(false);
-              setIsActiveMonthsReviews(false);
-              setIsActiveFinalReviews(false);
-              setIsActiveSolveExams(false);
+              setIsActiveLevels(!isActiveLevels);
+              setIsActiveAboutUs(false);
+              setIsActiveConectUs(false);
        };
-       const handleClickDuties = () => {
-              setIsActiveDuties(!isActiveDuties);
+       const handleClickAboutUs = () => {
+              setIsActiveAboutUs(!isActiveAboutUs);
               setIsActiveHome(false);
-              setIsActiveCurricula(false);
-              setIsActiveLiveClasses(false);
-              setIsActiveMonthsReviews(false);
-              setIsActiveFinalReviews(false);
-              setIsActiveSolveExams(false);
+              setIsActiveLevels(false);
+              setIsActiveConectUs(false);
        };
-       const handleClickLiveClasses = () => {
-              setIsActiveLiveClasses(!isActiveLiveClasses);
+       const handleClickConectUs = () => {
+              setIsActiveConectUs(!isActiveConectUs);
               setIsActiveHome(false);
-              setIsActiveCurricula(false);
-              setIsActiveDuties(false);
-              setIsActiveMonthsReviews(false);
-              setIsActiveFinalReviews(false);
-              setIsActiveSolveExams(false);
-       };
-       const handleClickMonthsReviews = () => {
-              setIsActiveMonthsReviews(!isActiveMonthsReviews);
-              setIsActiveHome(false);
-              setIsActiveCurricula(false);
-              setIsActiveDuties(false);
-              setIsActiveLiveClasses(false);
-              setIsActiveFinalReviews(false);
-              setIsActiveSolveExams(false);
-       };
-       const handleClickFinalReviews = () => {
-              setIsActiveFinalReviews(!isActiveFinalReviews);
-              setIsActiveHome(false);
-              setIsActiveCurricula(false);
-              setIsActiveDuties(false);
-              setIsActiveLiveClasses(false);
-              setIsActiveMonthsReviews(false);
-              setIsActiveSolveExams(false);
-       };
-       const handleClickSolveExams = () => {
-              setIsActiveSolveExams(!isActiveSolveExams);
-              setIsActiveHome(false);
-              setIsActiveCurricula(false);
-              setIsActiveDuties(false);
-              setIsActiveLiveClasses(false);
-              setIsActiveMonthsReviews(false);
-              setIsActiveFinalReviews(false);
+              setIsActiveLevels(false);
+              setIsActiveAboutUs(false);
        };
        return (
               <>
-                     <div className="w-full h-full mt-4 mb-8 flex justify-end">
-                            <div className="MenuSide w-5/6 flex flex-col items-start gap-y-4">
-                                   <NavLink to="/DashboardStudent" onClick={handleClickHome} className="w-full flex px-0 py-2 items-center justify-start gap-x-5 text-secoundColor text-xl font-medium ">
-                                          <HomeIcon isActive={isActiveHome} />
-                                          <span>الرئيسيه</span>
-                                   </NavLink>
-                                   <NavLink to="/Curricula" onClick={handleClickCurricula} className="w-full flex px-0 py-2 items-center justify-start gap-x-5 text-secoundColor text-xl font-medium">
-                                          <CurriculaIcon isActive={isActiveCurricula} />
-                                          <span>مناهج</span>
-                                   </NavLink>
-                                   <NavLink to="/Duties" onClick={handleClickDuties} className="w-full flex px-0 py-2 items-center justify-start gap-x-5 text-secoundColor text-xl font-medium">
-                                          <DutiesIcon isActive={isActiveDuties} />
-                                          <span>واجبات</span>
-                                   </NavLink>
-                                   <NavLink to="/LiveClasses" onClick={handleClickLiveClasses} className="w-full flex px-0 py-2 items-center justify-start gap-x-5 text-secoundColor text-xl font-medium">
-                                          <LiveClassesIcon isActive={isActiveLiveClasses} />
-                                          <span>حصص لايف</span>
-                                   </NavLink>
-                                   <NavLink to="/MonthsReviews" onClick={handleClickMonthsReviews} className="w-full flex px-0 py-2 items-center justify-start gap-x-5 text-secoundColor text-xl font-medium">
-                                          <MonthsReviewsIcon isActive={isActiveMonthsReviews} />
-                                          <span>مراجعات شهور </span>
-                                   </NavLink>
-                                   <NavLink to="/FinalReviews" onClick={handleClickFinalReviews} className="w-full flex px-0 py-2 items-center justify-start gap-x-5 text-secoundColor text-xl font-medium">
-                                          <FinalReviewsIcon isActive={isActiveFinalReviews} />
-                                          <span>مراجعه نهائيه</span>
-                                   </NavLink>
-                                   <NavLink to="/SolveExams" onClick={handleClickSolveExams} className="w-full flex px-0 py-2 items-center justify-start gap-x-5 text-secoundColor text-xl font-medium">
-                                          <SolveExamsIcon isActive={isActiveSolveExams} />
-                                          <span>حل امتحانات</span>
-                                   </NavLink>
-                            </div>
+                     <div className="w-full LinksHomeDS flex flex-row-reverse items-center justify-between ">
+                            <NavLink to="/" onClick={handleClickHome} className="w-full flex px-0 py-2 items-center justify-center gap-x-5 text-black text-xl font-medium ">
+                                   <span>الرئيسيه</span>
+                            </NavLink>
+                            <NavLink to="/Levels" onClick={handleClickLevels} className="w-full flex px-0 py-2 items-center justify-center gap-x-5 text-black text-xl font-medium">
+                                   <span>المراحل</span>
+                            </NavLink>
+                            <NavLink to="/AboutUs" onClick={handleClickAboutUs} className="w-full flex px-0 py-2 items-center justify-center gap-x-5 text-black text-xl font-medium">
+                                   <span>من نحن </span>
+                            </NavLink>
+                            <NavLink to="/ConectUs" onClick={handleClickConectUs} className="w-full flex px-0 py-2 items-center justify-center gap-x-5 text-black text-xl font-medium">
+                                   <span>اتصل بنا </span>
+                            </NavLink>
                      </div>
               </>
        );
 };
 
-export default MenuSide;
+export default LinksHomeDS;
