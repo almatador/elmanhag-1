9e0a5b381d4a44829ea82484b68c77056c8b8532
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-07-29 15:48:05 +0300

    New Update A

--- src/Components/HeaderHome.jsx
+++ src/Components/HeaderHome.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import Logo from '../Assets/Images/logoSidebar'
 import { Link } from 'react-router-dom'
+import LinksHomeDS from './LinksHomeDS'
 
 const HeaderHome = () => {
        return (
               <>
-                     <div className="w-5/6 m-auto flex flex-row-reverse items-center justify-between">
+                     <div className="w-full m-auto flex flex-row-reverse items-center justify-around">
 
-                            <div className="w-1/4">
-                                   <Logo Width="100%" />
+                            <div className="w-1/4 flex items-center justify-center">
+                                   <Logo />
                             </div>
-                            {/* <div className=""></div> */}
-                            <div className="p-3 text-xl text-secoundColor bg-mainBgColor rounded-2xl"><Link to={"./login"}>login</Link></div>
+                            <div className="w-2/4 flex items-center justify-center"><LinksHomeDS /></div>
+                            <div className="w-1/4 flex items-center justify-center"><Link to={"./login"} className="p-3 text-xl text-secoundColor bg-mainBgColor rounded-2xl">login</Link></div>
                      </div>
               </>
        )
