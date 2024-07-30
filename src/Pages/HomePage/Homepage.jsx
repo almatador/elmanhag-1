ae0253f0b00593491863211fa97941960c37fd2b
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-07-30 10:10:03 +0300

    New Update A

--- src/Pages/HomePage/Homepage.jsx
+++ src/Pages/HomePage/Homepage.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Homepage = () => {
        return (
-              <div>Homepage</div>
+              <>
+                     <div className="w-11/12 m-auto flex flex-row-reverse items-center justify-center">Homepage</div>
+              </>
        )
 }
 
