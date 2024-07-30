1541d76f1d0dac93aea4baff1af63abe79f75b8c
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-07-30 10:09:59 +0300

    New Update A

--- src/Components/HeaderHome.jsx
+++ src/Components/HeaderHome.jsx
@@ -6,13 +6,16 @@ import LinksHomeDS from './LinksHomeDS'
 const HeaderHome = () => {
        return (
               <>
-                     <div className="w-full m-auto flex flex-row-reverse items-center justify-around">
+                     <div className="w-11/12 m-auto flex flex-row-reverse items-center justify-between">
 
-                            <div className="w-1/4 flex items-center justify-center">
-                                   <Logo />
+                            <div className="w-1/5 flex items-center justify-center">
+                                   <Link to={"/"}><Logo Width={200} height={100} /></Link>
                             </div>
-                            <div className="w-2/4 flex items-center justify-center"><LinksHomeDS /></div>
-                            <div className="w-1/4 flex items-center justify-center"><Link to={"./login"} className="p-3 text-xl text-secoundColor bg-mainBgColor rounded-2xl">login</Link></div>
+                            <div className="w-2/5 flex items-center justify-center"><LinksHomeDS /></div>
+                            <div className="w-1/4 flex flex-row-reverse items-center justify-between">
+                                   <Link to={"/signUp"} className="px-4 py-3 text-2xl  text-secoundColor bg-mainColor rounded-2xl">انشاء حساب </Link>
+                                   <Link to={"/login"} className="px-3 text-xl text-mainColor font-primaryeMedium rounded-2xl">تسجيل الدخول</Link>
+                            </div>
                      </div>
               </>
        )
