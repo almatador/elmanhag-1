160114f5d0f507db2b7b9dd9e0f3c5390b479ce8
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-07-30 10:09:49 +0300

    New Update A

--- src/Components/FooterHome.jsx
+++ src/Components/FooterHome.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const FooterHome = () => {
-  return (
-    <div>FooterHome</div>
-  )
+       return (
+              <>
+                     <div className="w-11/12 m-auto flex flex-row-reverse items-center justify-center">FooterHome</div>
+              </>
+       )
 }
 
 export default FooterHome
