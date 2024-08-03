77cbff03f59a758af5233928326346547f905508
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-08-03 10:25:48 +0300

    New UPdate A

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,18 @@ export default {
       colors: {
         mainColor: "#D01025",
         secoundColor: "#fff",
+        thirdColor: "#6B6B6B",
       },
       backgroundColor: {
         mainBgColor: "#7E7D7D",
         secoundBgColor: "#cccccc",
+      },
+      screens: {
+        // sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        // "2xl": "1536px",
       },
     },
   },
