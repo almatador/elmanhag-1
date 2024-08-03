798c07a597c67b6690e33eb87fd04f0406e36987
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-08-03 10:26:05 +0300

    New UPdate A

---
+++ src/Components/Text.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TextTitle = ({ text, size, color, font }) => {
+       return (
+              <span
+                     className={`text-${size} text-${color} font-${font} `}
+              >
+                     {text}
+              </span>
+       );
+};
+
+export default TextTitle;
