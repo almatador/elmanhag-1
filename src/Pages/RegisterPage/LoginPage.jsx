a95a8d975255adbccc9c1b256391ae2ce2f3df0a
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-08-03 10:26:15 +0300

    New UPdate A

--- src/Pages/RegisterPage/LoginPage.jsx
+++ src/Pages/RegisterPage/LoginPage.jsx
@@ -1,11 +1,15 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { Navigate, NavLink, useNavigate } from 'react-router-dom';
 import Loading from '../../Components/Loading';
 import { useAuth } from '../../Context/Auth';
+import TextTitle from '../../Components/Text';
+import InputCustom from '../../Components/InputCustom';
 
 const LoginPage = () => {
        const auth = useAuth();
+
+       const [show, setShow] = useState(false)
 
        const [email, setEmail] = useState('');
        const [password, setPassword] = useState('');
@@ -27,6 +31,8 @@ const LoginPage = () => {
        }, [data]);
 
        const handleSubmit = async (event) => {
+              console.log(email)
+              console.log(password)
               event.preventDefault();
 
               // Ensure email and password are defined
@@ -72,50 +78,72 @@ const LoginPage = () => {
               )
        }
 
-
-       return (
-              <>
-
-                     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-                            <div className="w-full max-w-md p-8 space-y-6 bg-white rounded shadow-md">
-                                   <h2 className="text-2xl font-bold text-center">Login Page</h2>
-                                   <form onSubmit={handleSubmit} className="space-y-6">
-                                          <div>
-                                                 <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
-                                                 <input
-                                                        id="email"
-                                                        type="email"
-                                                        value={email}
-                                                        onChange={(e) => setEmail(e.target.value)}
-                                                        required
-                                                        className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                                                 />
-                                          </div>
-                                          {/* <Loading /> */}
-                                          <div>
-                                                 <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
-                                                 <input
-                                                        id="password"
-                                                        type="password"
-                                                        value={password}
-                                                        onChange={(e) => setPassword(e.target.value)}
-                                                        required
-                                                        className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                                                 />
-                                          </div>
-                                          <div>
-                                                 <button
-                                                        type="submit"
-                                                        className="w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 rounded hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                                                 >
-                                                        Login
-                                                 </button>
-                                          </div>
-                                   </form>
-                                   {error && <div className="text-red-500">{error}</div>}
-                                   {data && <div>{data} sss</div>}
-                            </div>
+       if (type === "password") {
+              return (<>
+                     <div className="relative w-full">
+                            <input type={show ? "text" : "password"} placeholder={placeholder} className=' w-full border rounded-2xl border-mainColor outline-none px-2 py-3 text-2xl font-normal text-thirdColor' required />
+                            {show ? <IoMdEye className='absolute top-4 right-2 text-2xl text-mainColor cursor-pointer' onClick={() => { setShow(!show) }} /> : <IoMdEyeOff className='absolute top-4 right-2 text-2xl text-mainColor cursor-pointer' onClick={() => setShow(!show)} />}
                      </div>
+              </>)
+       }
+       return (
+              // <>
+
+              //        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+              //               <div className="w-full max-w-md p-8 space-y-6 bg-white rounded shadow-md">
+              //                      <h2 className="text-2xl font-bold text-center">Login Page</h2>
+              //                      <form onSubmit={handleSubmit} className="space-y-6">
+              //                             <div>
+              //                                    <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
+              //                                    <input
+              //                                           id="email"
+              //                                           type="email"
+              //                                           value={email}
+              //                                           onChange={(e) => setEmail(e.target.value)}
+              //                                           required
+              //                                           className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              //                                    />
+              //                             </div>
+              //                             {/* <Loading /> */}
+              //                             <div>
+              //                                    <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+              //                                    <input
+              //                                           id="password"
+              //                                           type="password"
+              //                                           value={password}
+              //                                           onChange={(e) => setPassword(e.target.value)}
+              //                                           required
+              //                                           className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              //                                    />
+              //                             </div>
+              //                             <div>
+              //                                    <button
+              //                                           type="submit"
+              //                                           className="w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 rounded hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              //                                    >
+              //                                           Login
+              //                                    </button>
+              //                             </div>
+              //                      </form>
+              //                      {error && <div className="text-red-500">{error}</div>}
+              //                      {data && <div>{data} sss</div>}
+              //               </div>
+              //        </div>
+              // </>
+              <>
+                     <form onSubmit={handleSubmit} className="w-full flex flex-col items-start justify-center gap-4">
+                            <span className='text-thirdColor text-2xl font-medium'>Come on, log in</span>
+                            <div className="w-full flex flex-col gap-6 items-end">
+                                   <InputCustom type={"email"} placeholder={"Email"} value={email} onChange={(e) => setEmail(e.target.value)} />
+                                   <InputCustom type={"password"} placeholder={"Password"} value={password} onChange={(e) => setPassword(e.target.value)} />
+
+                                   <NavLink className="border-b-2 pb-1 border-mainColor text-2xl font-medium text-mainColor mb-6">Forget password?</NavLink>
+
+                                   {error && <div className="w-full text-mainColor text-center text-2xl mb-4 font-bold">{error}</div>}
+
+                            </div>
+                            <button type="submit" className="w-full text-center text-2xl font-medium text-secoundColor px-6 py-3 bg-mainColor rounded-2xl">Log in</button>
+                     </form>
               </>
        );
 };
