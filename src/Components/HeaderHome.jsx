572064436bf868df13097858923acb9a00195040
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-08-04 15:27:04 +0300

    New Update A

--- src/Components/HeaderHome.jsx
+++ src/Components/HeaderHome.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Logo from '../Assets/Images/logoBlack'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import LinksHomeDS from './LinksHomeDS'
 import { useAuth } from '../Context/Auth'
 
 const HeaderHome = () => {
-       const auth = useAuth()
+       const auth = useAuth();
+       const navigate = useNavigate();
+
+       const handleLogout = () => {
+              auth.logout();
+       }
+       const handleGoBack = () => {
+              navigate(-1, { replace: true });
+       }
        return (
               <>
                      <div className="w-11/12 m-auto flex flex-row-reverse items-center justify-between">
@@ -15,11 +23,17 @@ const HeaderHome = () => {
                             </div>
                             <div className="w-2/5 flex items-center justify-center"><LinksHomeDS /></div>
                             <div className="w-1/4 flex flex-row-reverse items-center justify-between">
-                                   {!auth.user && <>
+                                   {/* {auth.user &&
+                                          <button className="px-4 py-3 text-2xl  text-secoundColor bg-mainColor rounded-2xl" onClick={() => auth.logout()}>تسجيل الخروج</button>
+                                   } */}
+                                   {!auth.user ? <>
                                           <Link to={"/authentication/signUp"} className="px-4 py-3 text-2xl  text-secoundColor bg-mainColor rounded-2xl">انشاء حساب </Link>
                                           <Link to={"/authentication/login"} className="px-3 text-xl text-mainColor font-primaryeMedium rounded-2xl">تسجيل الدخول</Link>
+                                   </> : <><button type='button' className="px-4 py-3 mx-auto text-2xl  text-secoundColor bg-mainColor rounded-2xl" onClick={handleLogout}>Log Out</button>
+                                          <button type='button' className="px-4 py-3 mx-auto text-2xl  text-secoundColor bg-mainColor rounded-2xl" onClick={handleGoBack}>Dashboard</button>
                                    </>
-                                   }</div>
+                                   }
+                            </div>
                      </div>
               </>
        )
