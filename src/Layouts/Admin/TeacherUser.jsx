afbf7999c339cf6ffb7eda78fa41eecffd955bad
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-08-05 16:33:21 +0300

    New Update A

---
+++ src/Layouts/Admin/TeacherUser.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TeacherUser = () => {
+  return (
+    <div>TeacherUser</div>
+  )
+}
+
+export default TeacherUser
--- src/Layouts/Admin/UserAD.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const UserAD = () => {
-       return (
-              <div>UserAD</div>
-       )
-}
-
-export default UserAD
