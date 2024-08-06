fd89b37763739d29005cf23ce284251d80437a36
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-08-06 16:24:44 +0300

    New Update A

--- src/Pages/RegisterPage/LoginAdmin.jsx
+++ src/Pages/RegisterPage/LoginAdmin.jsx
@@ -79,14 +79,6 @@ const LoginAdmin = () => {
               )
        }
 
-       if (type === "password") {
-              return (<>
-                     <div className="relative w-full">
-                            <input type={show ? "text" : "password"} placeholder={placeholder} className=' w-full border rounded-2xl border-mainColor outline-none px-2 py-3 text-2xl font-normal text-thirdColor' required />
-                            {show ? <IoMdEye className='absolute top-4 right-2 text-2xl text-mainColor cursor-pointer' onClick={() => { setShow(!show) }} /> : <IoMdEyeOff className='absolute top-4 right-2 text-2xl text-mainColor cursor-pointer' onClick={() => setShow(!show)} />}
-                     </div>
-              </>)
-       }
        return (
               <>
                      <div className="flex flex-col lg:flex-row  items-center justify-center lg:justify-between w-full h-screen">
