ba9bc9662fa601b2105fab7d3618859416f9a5c6
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-08-11 15:49:53 +0300

    New Update A

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -5,7 +5,7 @@ const Sidebar = () => {
        return (
               <div className="relative w-4/12 lg:w-3/12 xl:w-2/12 border-r-2 overflow-hidden flex items-center justify-center bg-white gap-y-6">
 
-                     <aside className="fixed top- 0  z-10 h-screen flex flex-col items-center gap-y-2">
+                     <aside className="fixed top-0  z-10 h-screen flex flex-col items-center gap-y-2">
                             <div className="w-full flex items-center justify-center mt-2">
                                    <Logo Width={"100%"} Height={80} />
                             </div>
