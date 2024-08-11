3bd53b7947634c36c8af5300c1b53b26a72734e3
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-08-11 15:49:42 +0300

    New Update A

--- src/Components/InputCustom.jsx
+++ src/Components/InputCustom.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 import { IoMdEye, IoMdEyeOff } from "react-icons/io";
 
-const InputCustom = ({ type, placeholder, value, onChange }) => {
+const InputCustom = ({ type, borderColor = "mainColor", placeholder, value, onChange }) => {
        const [show, setShow] = useState(false)
        if (type === "password") {
               return (<>
                      <div className="relative w-full">
-                            <input type={show ? "text" : "password"} placeholder={placeholder} className=' w-full border-2 rounded-2xl border-mainColor outline-none px-2 py-3 text-2xl font-normal text-thirdColor' value={value}
+                            <input type={show ? "text" : "password"} placeholder={placeholder} className={`w-full border-2 rounded-2xl outline-none px-2 py-3 text-2xl font-normal text-thirdColor border-${borderColor}`} value={value}
                                    onChange={onChange} required />
                             {show ? <IoMdEye className='absolute top-4 right-2 text-2xl text-mainColor cursor-pointer' onClick={() => { setShow(!show) }} /> : <IoMdEyeOff className='absolute top-4 right-2 text-2xl text-mainColor cursor-pointer' onClick={() => setShow(!show)} />}
                      </div>
@@ -14,7 +14,7 @@ const InputCustom = ({ type, placeholder, value, onChange }) => {
        }
        return (
               <>
-                     <input type={type} placeholder={placeholder} className='w-full border-2 rounded-2xl border-mainColor outline-none px-2 py-3 text-2xl font-normal text-thirdColor' value={value}
+                     <input type={type} placeholder={placeholder} className={`w-full border-2 rounded-2xl border-${borderColor} outline-none px-2 py-3 text-2xl font-normal text-thirdColor`} value={value}
                             onChange={onChange} required />
               </>
 
