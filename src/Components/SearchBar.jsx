b62b90636d060f3847ce295c5aa8fde5660a3427
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-08-13 16:27:36 +0300

    New Update A

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { IoSearch } from 'react-icons/io5'
 
-const SearchBar = ({ bg }) => {
+const SearchBar = ({ bg ,value}) => {
        return (
               <div className='w-full relative'>
-                     <input type="text" name="search" className={`bg-${bg} w-full h-full pl-12 py-3 rounded-3xl outline-none font-medium text-thirdColor`} placeholder='Search' />
+                     <input type="text" name="search" value={value} className={`bg-${bg} w-full h-full pl-12 py-3 rounded-3xl outline-none font-medium text-thirdColor`} placeholder='Search' />
                      <IoSearch className='absolute top-3 left-4 text-mainColor font-bold text-xl' />
 
               </div>
