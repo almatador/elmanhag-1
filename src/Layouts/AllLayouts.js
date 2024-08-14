5bdfad9e7fc12f80807e0f5b048f9daa60a57e4d
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-08-14 16:04:06 +0300

    New Update  A

--- src/Layouts/AllLayouts.js
+++ src/Layouts/AllLayouts.js
@@ -2,14 +2,20 @@
 /* Admin Layout */
 export { default as LayoutAdmin } from "./Admin/LayoutAdmin";
 export { default as DashboardAD } from "./Admin/DashboardAD";
+/* USer */
 export { default as StudentUser } from "./Admin/StudentUser";
+export { default as AddStudentpage } from "./Admin/AddStydentPage";
+export { default as EditProfileStudent } from "./Admin/EditeProfileStudent";
 export { default as ParentUser } from "./Admin/ParentUser";
 export { default as TeacherUser } from "./Admin/TeacherUser";
 export { default as AdminUser } from "./Admin/AdminUser";
+/* ////USer */
+/* Education */
 export { default as CategoriesEducation } from "./Admin/CategoriesEducation";
 export { default as SubjectEducation } from "./Admin/SubjectEducation";
 export { default as BundlesEducation } from "./Admin/BundlesEducation";
 export { default as QuestionsBankEducation } from "./Admin/QuestionsBankEducation";
+/* ////Education */
 export { default as HomeWorkAD } from "./Admin/HomeWorkAD";
 export { default as RevisionAD } from "./Admin/RevisionAD";
 export { default as ExamsAD } from "./Admin/ExamsAD";
@@ -19,7 +25,13 @@ export { default as FinancialAD } from "./Admin/FinancialAD";
 export { default as AffiliateAD } from "./Admin/AffiliateAD";
 export { default as SupportAD } from "./Admin/SupportAD";
 export { default as ReportsAD } from "./Admin/ReportsAD";
-export { default as SettingAD } from "./Admin/SettingAD";
+/* Setting */
+export { default as AdminRolesAD } from "./Admin/AdminRolesAD";
+export { default as CountriesAD } from "./Admin/CountriesAD";
+export { default as CitiesAD } from "./Admin/CitiesAD";
+export { default as ParentRelationAD } from "./Admin/ParentRelationAD";
+export { default as OperationsAD } from "./Admin/OperationsAD";
+/* ////Setting */
 export { default as NoticeBoardAD } from "./Admin/NoticeBoardAD";
 
 /* Teachers Layout */
--- src/Pages/Admin/StudentPage/EditProfilePage.jsx
+++ src/Pages/Admin/StudentPage/EditProfilePage.jsx
@@ -1,19 +1,10 @@
 import React from 'react'
-import { NavLink, Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 
 const EditProfilePage = () => {
        return (
               <>
-                     {/* <div className="w-11/12 mx-auto"> */}
-                            <div className="LinksProfile flex flex-wrap w-full justify-between mb-8 pt-5">
-                                   <NavLink to={"profile"} className="w-1/6 text-thirdColor text-center text-2xl font-medium">profile</NavLink>
-                                   <NavLink to={"parent"} className="w-1/6 text-thirdColor text-center text-2xl font-medium">parent</NavLink>
-                                   <NavLink to={"Purchases"} className="w-1/6 text-thirdColor text-center text-2xl font-medium">Purchases</NavLink>
-                                   <NavLink to={"Progress"} className="w-1/6 text-thirdColor text-center text-2xl font-medium">Progress</NavLink>
-                                   <NavLink to={"loginHistory"} className="w-1/6 text-thirdColor text-center text-2xl font-medium">loginHistory</NavLink>
-                            </div>
-                            <Outlet />
-                     {/* </div> */}
+                     <Outlet />
               </>
        )
 }
