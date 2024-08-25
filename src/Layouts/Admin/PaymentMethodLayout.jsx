e7db6b146a0bd6b1817983888179ae706dd7aeb3
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-08-25 11:20:59 +0300

    New UPdate A

--- src/Layouts/Admin/PaymentMethodLayout.jsx
+++ src/Layouts/Admin/PaymentMethodLayout.jsx
@@ -1,25 +1,12 @@
 import React from 'react'
-import HeaderPageSection from '../../Components/HeaderPageSection'
-import { Link, useNavigate } from 'react-router-dom'
 import { PaymentMethodPage } from '../../Pages/AllPages'
-import { ButtonAdd } from '../../Components/Button'
+import TitleHeader from '../../Components/TitleHeader'
 
 const PaymentMethodLayout = () => {
-       const navigate = useNavigate();
-       const handleGoBack = () => {
-              navigate(-1); // This navigates back to the previous page
-       };
        return (
               <>
-                     <div className="w-full flex items-start flex-col gap-y-3">
-                            <HeaderPageSection handleClick={handleGoBack} name={'PaymentMethod'} />
-
-                            <div className="sm:w-full xl:w-1/12">
-                                   <Link to="add">
-                                          <ButtonAdd Text={"Add"} BgColor={"white"} Color={"thirdColor"} iconColor="mainColor" Size={"xl"} />
-                                   </Link>
-                            </div>
-
+                     <div className="flex flex-col items-center gap-y-4">
+                            <TitleHeader text={"PaymentMethod"} spaceBottom={3} />
                             <PaymentMethodPage />
                      </div>
               </>
--- src/Pages/Admin/SettingPage/PaymentMethod/PaymentMethodPage.jsx
+++ src/Pages/Admin/SettingPage/PaymentMethod/PaymentMethodPage.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { ButtonAdd } from '../../../../Components/Button'
 
 const PaymentMethodPage = () => {
-  return (
-    <div>PaymentMethodPage</div>
-  )
+       return (
+              <>
+                     <div className="w-full flex flex-col gap-y-3">
+                            <div className="sm:w-full xl:w-1/12">
+                                   <Link to="add">
+                                          <ButtonAdd Text={"Add"} BgColor={"white"} Color={"thirdColor"} iconColor="mainColor" Size={"xl"} />
+                                   </Link>
+                            </div>
+                     </div>
+              </>
+       )
 }
 
 export default PaymentMethodPage
