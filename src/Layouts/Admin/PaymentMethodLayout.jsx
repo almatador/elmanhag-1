f4ee672aaf32572933d6a08a3fea3a7e71034057
Basantsaeed <89366449+Basantsaed@users.noreply.github.com>
2024-08-25 11:45:04 +0300

    commit

--- src/Layouts/Admin/PaymentMethodLayout.jsx
+++ src/Layouts/Admin/PaymentMethodLayout.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
-import { PaymentMethodPage } from '../../Pages/AllPages'
-import TitleHeader from '../../Components/TitleHeader'
+import React from "react";
+import TitleHeader from "../../Components/TitleHeader";
+import { ButtonAdd } from "../../Components/Button";
+import { Link, useNavigate } from "react-router-dom";
+import { FaAngleLeft } from "react-icons/fa";
+import { PaymentMethodPage } from "../../Pages/AllPages";
+import HeaderPageSection from "../../Components/HeaderPageSection";
 
 const PaymentMethodLayout = () => {
+       const navigate = useNavigate();
+  const handleGoBack = () => {
+         navigate(-1); // This navigates back to the previous page
+  };
        return (
               <>
-                     <div className="flex flex-col items-center gap-y-4">
-                            <TitleHeader text={"PaymentMethod"} spaceBottom={3} />
-                            <PaymentMethodPage />
+              <div className="w-full flex items-start flex-col gap-y-3">
+                     <HeaderPageSection handleClick={handleGoBack} name={'Payment Method'} />
+
+                     <div className="sm:w-full xl:w-1/12">
+                            <Link to="add">
+                                   <ButtonAdd Text={"Add"} BgColor={"white"} Color={"thirdColor"} iconColor="mainColor" Size={"xl"} />
+                            </Link>
                      </div>
-              </>
+
+                     <PaymentMethodPage />
+              </div>
+              </>  
+       
        )
 }
 
 export default PaymentMethodLayout
+
+
+
+
