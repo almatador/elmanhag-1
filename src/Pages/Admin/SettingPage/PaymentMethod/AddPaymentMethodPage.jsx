3543151cebd0ba699c766262c1780cb41b894fde
Basantsaeed <89366449+Basantsaed@users.noreply.github.com>
2024-08-26 11:46:20 +0300

    fcommit

--- src/Pages/Admin/SettingPage/PaymentMethod/AddPaymentMethodPage.jsx
+++ src/Pages/Admin/SettingPage/PaymentMethod/AddPaymentMethodPage.jsx
@@ -156,7 +156,7 @@ const AddPaymentMethodPage = () => {
                     <div className="flex items-center gap-x-4 lg:w-[30%] sm:w-full">
                         <span className="text-2xl text-thirdColor font-medium">Active:</span>
                         <div>
-                            <CheckBox handleClick={handleClick} />
+                            <CheckBox handleClick={handleClick} checked={paymentActive}/>
                         </div>
                     </div>
                 </div>
